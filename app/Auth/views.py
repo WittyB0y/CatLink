145c0b1fbcf49e68f0b424705174b9f24cd12bd6
Artsem <bobrovnik42@gmail.com>
2024-04-17 18:56:09 +0300

    add register for user with jwt token && endpoints for login, refresh token, verify token

---
+++ app/Auth/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
