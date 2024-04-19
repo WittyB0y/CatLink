650bbacf65f66fc57369c2441074235bf4c33455
Artsem <bobrovnik42@gmail.com>
2024-04-19 20:37:56 +0300

    refactor api and serializer for collection

--- app/Collection/serializers.py
+++ app/Collection/serializers.py
@@ -3,6 +3,20 @@ from Collection.models import Collection
 from Link.serializers import LinkSerializer
 
 
+class CreateCollectionSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Collection
+        fields = ['name', 'description', 'links', 'user']
+
+    def links(self, obj):
+        """to get list of ids"""
+        links_obj = obj.links.all()
+        return [link.id for link in links_obj]
+
+
 class CollectionSerializer(serializers.ModelSerializer):
     links = LinkSerializer(many=True, read_only=True)
 
@@ -12,7 +26,6 @@ class CollectionSerializer(serializers.ModelSerializer):
 
 
 class UpdateCollectionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Collection
         fields = ['name', 'description', 'links']
--- app/Collection/views.py
+++ app/Collection/views.py
@@ -3,12 +3,17 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from Link.models import Link
 from .models import Collection
-from .serializers import CollectionSerializer, UpdateCollectionSerializer
+from .serializers import (
+    CollectionSerializer,
+    UpdateCollectionSerializer,
+    CreateCollectionSerializer
+)
 
 
 class CollectionViewSet(viewsets.ModelViewSet):
     queryset = Collection.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ["get", "post", "put", "delete"]
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
@@ -22,8 +27,10 @@ class CollectionViewSet(viewsets.ModelViewSet):
         match method:
             case 'list':
                 return CollectionSerializer
-            case 'partial_update':
+            case 'update':
                 return UpdateCollectionSerializer
+            case 'create':
+                return CreateCollectionSerializer
             case _:
                 return CollectionSerializer
 
@@ -39,7 +46,7 @@ class CollectionViewSet(viewsets.ModelViewSet):
         # add each link to db
         if link_ids:
             for link_id in link_ids:
-                link = get_object_or_404(Link, id=link_id, user=request.user)
-                collection.links.add(link)
+                link = get_object_or_404(Link, id=link_id, user=request.user.id)
+                collection.links.set([link])
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
